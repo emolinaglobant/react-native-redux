512b0bdf2b789ed158310461e8fb800451abb3e6
cvergaramarrugo <setsuna_last@hotmail.com>
2019-08-09 17:49:22 -0500

    Pokedex list and pokedex details

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import PokedexList from './screens/PokedexList'
+import PokedexDetails from './screens/PokedexDetails'
+import { createStackNavigator, createAppContainer } from 'react-navigation';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const AppNavigator = createStackNavigator({
+  PokedexList: {
+    screen: PokedexList,
+    navigationOptions:{
+      title:'Pokedex 1.0.0'
+    } 
   },
+  PokedexDetails: {
+    screen: PokedexDetails,
+    navigationOptions:  {
+        title: 'Pokemon details'
+    }
+  }
 });
+
+export default createAppContainer(AppNavigator);
